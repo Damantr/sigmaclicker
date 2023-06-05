f5d342fecd2e9c359907a9eb7c1e8bd39438abde
Andruha <andreonepis@gmail.com>
2023-06-05 14:54:33 +0100

    bebra

--- sigmaclicker/MainWindow.xaml.cs
+++ sigmaclicker/MainWindow.xaml.cs
@@ -27,12 +27,54 @@ namespace sigmaclicker
         {
             InitializeComponent();
         }
-        public int count = 0;
+        public double count = 0;
+        public double countAddition = 1;
         private void sykabla_Click(object sender, RoutedEventArgs e)
         {
-            count++;
+            count += countAddition;
             bebra1.Text = Convert.ToString(count);
         }
-        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (count >= 5)
+            {
+                count -= 5;
+                bebra1.Text = Convert.ToString(count);
+                countAddition += 1.333;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (count >= 20)
+            {
+                count -= 20;
+                bebra1.Text = Convert.ToString(count);
+                countAddition += 5.133;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (count >= 50)
+            {
+                count -= 50;
+                bebra1.Text = Convert.ToString(count);
+                countAddition += 10.753;
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (count >= 100)
+            {
+                count -= 100;
+                bebra1.Text = Convert.ToString(count);
+                countAddition += 22.313;
+            }
+        }
+
+      
     }
 }
