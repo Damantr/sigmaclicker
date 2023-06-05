d4b3289b78cea6c50c231a46c5cb5931406f2c52
Andruha <andreonepis@gmail.com>
2023-06-05 14:20:04 +0100

    penos

--- sigmaclicker/MainWindow.xaml.cs
+++ sigmaclicker/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace sigmaclicker
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    ///penino 
+
     public partial class MainWindow : Window
     {
         public MainWindow()
